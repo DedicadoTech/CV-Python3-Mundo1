d731e14378e374b365789a7a050acd3f0e1f078f
Davi de Souza Silva <davi.silva41@fatec.sp.gov.br>
2021-08-19 11:30:58 -0300

    formatando raiz quadrada para mostrar apenas duas casas decimais

--- desafio/desafio06-Dobro_Triplo_Raiz.py
+++ desafio/desafio06-Dobro_Triplo_Raiz.py
@@ -4,9 +4,9 @@
 #o triplo de 85 vale 255.
 #a raiz quadrada de 85 é igual a 9.22.
 
-num = int(input("Digite um número"))
+num = int(input("Digite um número: "))
 print("O dobro de {} vale {}.\n"
       "O triplo de {} vale {}.\n"
-      "A raiz quadrada de {} é igual a {}".format(num,num*2,
+      "A raiz quadrada de {} é igual a {:.2f}".format(num,num*2,
                                                   num,num*3,
                                                   num,float(num) ** 0.5))#converte int em float
