3105558d3b20824d40506cebb3c4a4159d85636d
Davi de Souza Silva <davi.silva41@fatec.sp.gov.br>
2021-08-22 09:25:51 -0300

    desafio09-Tabuada.py ##Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada #Saída: #---------------------- #6 x 1 = 6 #6 x 2 = 12 #6 x 3 = 18 #.... #6 x 10 = 60 #--------------------- ##

--- desafio/desafio09-Tabuada.py
+++ desafio/desafio09-Tabuada.py
@@ -10,9 +10,9 @@
 ##
 num = int(input('Digite um número para ver sua tabuada: '))
 mult = int(input('Digite quantos multiplos desse número deseja visualizar: '))
-print('-'*52)
+print('-' * 52)
 i = 0 #índice iniciando com 0
 while i <= mult:
     print('|'+' {} x {} = {}'.format(num,i,num * i).center(50)+'|') ##usa o valor do índice como contador
     i = i+1
-print('-'* 52)
+print('-' * 52)
