bf132de01bb824c8a75c39bdeefd417a819aef8b
Davi <dedicadotech@gmail.com>
2022-12-27 19:52:44 -0300

    Adicionado ex10.py

---
+++ exercicios/ex10.py
@@ -0,0 +1,12 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e
+# mostre quantos dólares ela pode comprar
+# Considere US$1,00 = R$5,27
+
+carteira = float(input('Digite o valor que você tem na carteira:\n R$ '))
+dolar = carteira / 5.29
+#troca ponto por virgula
+#converte para float para string
+carteira = str(carteira).replace('.',',')
+
+print('Valor em Dolar US$ {:.2f}\nValor em Reais R$ {}'.format(dolar, carteira))
+#fim do programa
